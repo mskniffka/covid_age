db3351d6d1f48ad30ee6f8863bab04edbcbf0bc4
Donzowa <jessica_d.1994@yahoo.de>
2021-09-30 10:47:33 +0200

    Script for quality checks

--- R_checks/check_data_shapes.R
+++ R_checks/check_data_shapes.R
@@ -4,10 +4,10 @@ require(lubridate)
 require(covidAgeData)
 require(here)
 
-# data_source <- "input-data/COVerAGE-DB/Output_5.zip"
-# link_old <- "https://osf.io/7tnfh/download?version=135&displayName=Output_5-2021-01-13T07%3A22%3A39.845954%2B00%3A00.zip"
-# osf_retrieve_file("7tnfh") %>%
-#   osf_download(path = "input-data/COVerAGE-DB/", conflicts = "overwrite")
+data_source <- "input-data/COVerAGE-DB/Output_5.zip"
+link_old <- "https://osf.io/7tnfh/download?version=135&displayName=Output_5-2021-01-13T07%3A22%3A39.845954%2B00%3A00.zip"
+osf_retrieve_file("7tnfh") %>%
+osf_download(path = "Data", conflicts = "overwrite")
 
 dat <-  read_csv("Data/Output_5.zip",
                     skip = 3)
@@ -62,7 +62,7 @@ for(i in 1:max(cts$gr)){
   cts_t <- 
     cts %>% 
     filter(gr == i) %>% 
-    pull(Country)
+    dplyr::pull(Country)
   
   dat2 %>% 
     filter(Country %in% cts_t) %>% 
@@ -73,7 +73,7 @@ for(i in 1:max(cts$gr)){
       axis.text.x = element_text(size = 5)
     )
   
-  ggsave(paste0("quality_checks/deaths_210410_gr", i, ".png")) 
+  ggsave(paste0("R_checks/quality_checks/deaths_210816_gr", i, ".png")) 
 }
 
 
